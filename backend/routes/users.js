af6adb8f84b3310915989466da5420056d09607f
Reynard <natividad.reynard@gmail.com>
2020-07-10 10:06:04 +0800

    Finished backend from tutorial

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
-router.route('/').get((res, req) => {
+router.route('/').get((req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
